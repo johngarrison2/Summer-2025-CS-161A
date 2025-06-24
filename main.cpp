07639202761cf7f575ed9750c1d2446e787b0040
johngarrison2 <john.garrison2@pcc.edu>
2025-06-24 19:01:22 +0000

    add file

---
+++ main.cpp
@@ -0,0 +1,76 @@
+// ------------- FILE HEADER -------------
+// Author: 
+// Assignment:
+// Date:
+// Description:
+// Sources: 
+
+
+// ------------- ZYBOOKS SCORES -------------
+// https://learn.zybooks.com/zybook/PCCCS161ASummer25
+// Chapter: 
+// Participation: 
+// Challenge:
+// Labs:
+
+
+// ------------- DISCORD POSTS -------------
+// https://discord.com/invite/URYKKf8YHm
+// Count:
+// Links (Optional): 
+
+
+// ------------- DESIGN DOCUMENT -------------
+// A. INPUT [yes/no]: 
+// B. OUTPUT [yes/no]:
+// C. CALCULATIONS [yes/no]:
+// D. LOGIC and ALGORITHMS [yes/no]:
+//    (Optional) flow chart link or file name: 
+
+
+// ------------- TESTING -------------
+// PASS ALL GIVEN SAMPLE RUN TESTS [yes/no]: 
+// (Optional) Additional tests count:   
+
+
+// ------------- CODE -------------
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, World!" << endl;
+  return 0;
+}
+
+
+
+
+// ------------- DESIGN -------------
+/* 
+Program Name:
+
+Program Description:
+
+Design:
+A. INPUT
+Define the input variables including name data type. 
+
+B. OUTPUT
+Define the output variables including data types. 
+
+C. CALCULATIONS
+Describe calculations used by algorithms in step D.  
+List all formulas. 
+If there are no calculations needed, state there are no calculations.
+
+D. LOGIC and ALGORITHMS
+Design the logic of your program using pseudocode or flowcharts. 
+Use conditionals, loops, functions or array constructs.
+List the steps in transforming inputs into outputs. 
+
+
+SAMPLE RUNS
+Copy from assignment document.
+
+*/
